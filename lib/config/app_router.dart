789c958429dd439ec77168f8e1227b9d28acc8e3
atalayz <atalayzistovili@hotmail.com>
2022-02-23 20:30:36 +0300

    sayfa eklendı

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app/screens/alphan_yemek/alphan_yemek.dart';
 import 'package:flutter_recipe_app/screens/mainpage/home_screen.dart';
-import 'package:flutter_recipe_app/screens/umutcan_yemek/umutcan_yemek.dart';
 
 class AppRouter {
   static Route onGenerateRoute(RouteSettings settings) {
@@ -15,8 +14,6 @@ class AppRouter {
         return HomeScreen.route();
       case AlphanYemek.routeName:
         return AlphanYemek.route();
-      case UmutcanYemek.routeName:
-        return UmutcanYemek.route();
       default:
         return _errorRoute();
     }
